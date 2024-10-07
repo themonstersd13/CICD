9f0e0a288bb8cbc3f1b04ff82f80fe399a379c92
Aditya Aparadh <aditya.aparadh.0@gmail.com>
2024-10-07 18:44:38 +0530

    Fix

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI || "mongo://mongodb://localhost:27017/Tasks");
 
 app.use('/api', todoRoutes);
 
