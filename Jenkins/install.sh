66bb1954c50c78966881e0ec8b4503870ec5945c
Aditya Aparadh <aditya.aparadh.0@gmail.com>
2024-10-11 21:54:55 +0530

    Added resources

---
+++ Jenkins/install.sh
@@ -0,0 +1,58 @@
+# ----------------------------------------------------------------------------
+# JRE Install
+# ----------------------------------------------------------------------------
+sudo add-apt-repository universe
+sudo apt install openjdk-21-jre
+
+
+
+
+# ----------------------------------------------------------------------------
+# Jenkins Install
+# ----------------------------------------------------------------------------
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+
+
+
+# ----------------------------------------------------------------------------
+# Docker Install ( skip if already installed)
+# ----------------------------------------------------------------------------
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+
+
+
+# ----------------------------------------------------------------------------
+# Bun Install
+# ----------------------------------------------------------------------------
+
+sudo apt install unzip
+curl -fsSL https://bun.sh/install | bash
+
+
+# ----------------------------------------------------------------------------
+# Post Install ( May need to run manually again)
+# ----------------------------------------------------------------------------
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo usermod -aG docker jenkins
+sudo systemctl start jenkins
---
+++ README.md
@@ -0,0 +1,42 @@
+# CICD Hands On
+
+### Installation
+
+Firstly you need :
+- JRE 17 or 21
+- Docker and Docker Compose Plugin
+- Jenkins (LTS or Weekly)
+- BunJS
+
+Make sure you add `Jenkins` to `Docker` Group, and restart `jenkins.service`.
+
+Install script for above tools ( only for debian based distributions can be found inside Jenkins folder)
+
+
+
+### Setup
+
+1. Visit `http://localhost:8080`
+
+The initial password can be found by:
+```
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+```
+
+2. Then select **Install Recommended Plugins**. Wait for installation.
+
+3. After that, ignore user setup for now
+
+4. On Jenkins Dashboard, click on `New Item`
+
+5. Name the Item, and select type as **Pipeline**
+
+6. Set respective fields, and for groovy script, copy from this repo. Also set Git SCM polling to "* * * * *", which will poll every minute.
+
+7. Save pipeline.
+
+8. Build Manually.
+
+9. Visiting `http://localhost:5173` should now open the application.
+
+10. `docker ps` should show three running containers, for Frontend, Backend and Database (MongoDB).
